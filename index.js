ff5c1d2ad38052a45377217d0f874922ce5cca95
Vaishnavikm <91455687+Vaishnavikm@users.noreply.github.com>
2022-02-15 06:42:20 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-angular.module("inv",["ngroute"])
-.controller("invctrl", ()=>{
-    document.getElementById("variable").innerHTML = "inv"
+var app = angular.module('myApp', [], ['ui.bootstrap']);
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
 
-})
+        .when('/', {
+        templateUrl: 'today.html',
+        controller: 'FirstController'
+    })
 
-inv.config("homeRt", ($routeProvider)=>{
+    .when('/manipulate', {
+        templateUrl: 'product.html',
+        controller: 'SecondController'
+    })
 
-})
+    .when('/mechanicdata', {
+        templateUrl: 'productinfo.html',
+        controller: 'ThirdController'
+    })
 
-document.getElementById("abc").addEventListener('keydown',(e)=>{
- console.log(e)
-})
+    .otherwise({
+        redirectTo: '/'
+    });
+});
+
+
+app.controller('FirstController', function($scope) {
+    $scope.message = 'All products are dispatched';
+});
+
+
+app.controller('SecondController', function($scope, $http) {
+    $http.get('https://vaishnavikm.github.io/json-file-/getproducts.json')
+        .success(function(response) {
+            $scope.names = response.products;
+        });
+});
+
+app.controller('ThirdController', function($scope, $http) {
+    $http.get('https://vaishnavikm.github.io/json-file-/getproducts.json')
+        .success(function(response) {
+            $scope.names = response.products;
+            $scope.rowlimit = 6;
+        });
+});
